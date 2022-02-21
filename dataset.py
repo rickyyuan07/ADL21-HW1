c23894715fea4932fc1e8c4d35688dd79f922164
rickyyuan07 <rickyyuan07@gmail.com>
2022-02-21 21:11:43 +0800

    finish simple dataset

--- dataset.py
+++ dataset.py
@@ -10,32 +10,40 @@ class SeqClsDataset(Dataset):
         self,
         data: List[Dict],
         vocab: Vocab,
-        label_mapping: Dict[str, int],
+        _label2idx: Dict[str, int],
         max_len: int,
     ):
         self.data = data
         self.vocab = vocab
-        self.label_mapping = label_mapping
-        self._idx2label = {idx: intent for intent, idx in self.label_mapping.items()}
+        self._label2idx = _label2idx
+        self._idx2label = {idx: intent for intent, idx in self._label2idx.items()}
         self.max_len = max_len
 
     def __len__(self) -> int:
         return len(self.data)
 
     def __getitem__(self, index) -> Dict:
-        instance = self.data[index]
-        return instance
+        # {'text': ..., 'intent': ..., 'id': ...}, i.g.
+        # "text": "i need you to book me a flight from ft lauderdale to houston on southwest",
+        # "intent": "book_flight",
+        # "id": "train-0"
+        return self.data[index]
 
     @property
     def num_classes(self) -> int:
-        return len(self.label_mapping)
+        return len(self._label2idx)
 
     def collate_fn(self, samples: List[Dict]) -> Dict:
         # TODO: implement collate_fn
-        raise NotImplementedError
+        texts, intents, ids = [], [], []
+        for sample in samples:
+            texts.append(sample['text'])
+            ids.append(sample['id'])
+            intents.append(sample['intent'])
+        return {'text': texts, 'intent': intents, 'id': ids}
 
     def label2idx(self, label: str):
-        return self.label_mapping[label]
+        return self._label2idx[label]
 
     def idx2label(self, idx: int):
         return self._idx2label[idx]
