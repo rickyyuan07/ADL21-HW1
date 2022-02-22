3b9defa729b8192e20768cbc79700fcebd7afc25
rickyyuan07 <rickyyuan07@gmail.com>
2022-02-22 14:11:34 +0800

    finish first version GRU model

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from typing import Dict
 
 import torch
 from torch.nn import Embedding
+import torch.nn as nn
 
 
 class SeqClassifier(torch.nn.Module):
@@ -16,7 +17,21 @@ class SeqClassifier(torch.nn.Module):
     ) -> None:
         super(SeqClassifier, self).__init__()
         self.embed = Embedding.from_pretrained(embeddings, freeze=False)
+        self.input_size = embeddings.shape[1]
         # TODO: model architecture
+        self.rnn = nn.GRU(
+            input_size=self.input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            dropout=dropout,
+            batch_first=True, # (batch_size, time_step, input)
+            bidirectional=bidirectional,
+        )
+        self.out = nn.Sequential(nn.Dropout(dropout),
+                      nn.Linear(in_features=hidden_size * 2, out_features=hidden_size * 2),
+                      nn.PReLU(),
+                      nn.Dropout(dropout),
+                      nn.Linear(in_features=hidden_size * 2, out_features=num_class))
 
     @property
     def encoder_output_size(self) -> int:
@@ -25,4 +40,9 @@ class SeqClassifier(torch.nn.Module):
 
     def forward(self, batch) -> Dict[str, torch.Tensor]:
         # TODO: implement model forward
-        raise NotImplementedError
+        # print(batch.shape) # (128, 75) = (batch_size, max_len)
+        x = self.embed(batch)
+        
+        r_out, _ = self.rnn(x, None)
+        out = self.out(r_out[:, -1, :])
+        return out
