ae73e65820bed82b937761a74e77fc98aff93a55
rickyyuan07 <rickyyuan07@gmail.com>
2022-03-22 19:58:30 +0800

    update trained model paths

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
-wget https://www.cmlab.csie.ntu.edu.tw/~rickyyuan/ADL/intent/intent_best.pt
-wget https://www.cmlab.csie.ntu.edu.tw/~rickyyuan/ADL/slot/slot_best.pt
+wget https://www.cmlab.csie.ntu.edu.tw/~rickyyuan/ADL/intent/best.pt
+wget https://www.cmlab.csie.ntu.edu.tw/~rickyyuan/ADL/slot/best.pt
